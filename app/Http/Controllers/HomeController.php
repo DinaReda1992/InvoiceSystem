e7ae73a253ab4a52bc4f37a3a8df0209c9d7c81c
Dina Reda <engdondon92@gmail.com>
2024-01-23 14:58:39 +0300

    statistics home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $count = Invoice::count();
+        return view('home',compact('count'));
     }
 }
